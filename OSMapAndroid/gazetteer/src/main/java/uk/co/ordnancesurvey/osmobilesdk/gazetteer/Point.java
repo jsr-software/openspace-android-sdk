24227658e5a3cf5c7be89337654ddb866175f9d2
Jim Reeves <jim@jsrsoftware.com>
2014-12-22 22:10:19 +0000

    Temporary copy of GIS classes into module

---
+++ OSMapAndroid/gazetteer/src/main/java/uk/co/ordnancesurvey/osmobilesdk/gazetteer/Point.java
@@ -0,0 +1,33 @@
+package uk.co.ordnancesurvey.osmobilesdk.gazetteer;
+
+public class Point {
+
+    public static final int WGS84 = 4326;
+    public static final int BNG = 27700;
+
+    private final double mX;
+    private final double mY;
+    private final int mSrid;
+
+    public Point(double x, double y, int srid) {
+        mX = x;
+        mY = y;
+        mSrid = srid;
+    }
+
+    public double getX() {
+        return mX;
+    }
+
+    public double getY() {
+        return mY;
+    }
+
+    public boolean isBng() {
+        return mSrid == BNG;
+    }
+
+    public double distanceTo(Point other) {
+        return Math.hypot(mX - other.getX(), mY - other.getY());
+    }
+}

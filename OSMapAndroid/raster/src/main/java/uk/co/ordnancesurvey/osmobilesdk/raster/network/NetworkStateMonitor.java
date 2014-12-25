962da84f0832fee28465f96d70ad2be6ff91a01c
Jim Reeves <jim@jsrsoftware.com>
2014-12-25 11:18:05 +0000

    Add package info

--- OSMapAndroid/raster/src/main/java/uk/co/ordnancesurvey/osmobilesdk/raster/network/NetworkStateMonitor.java
+++ OSMapAndroid/raster/src/main/java/uk/co/ordnancesurvey/osmobilesdk/raster/network/NetworkStateMonitor.java
@@ -1,25 +1,25 @@
 /**
- * OpenSpace Android SDK Licence Terms
- *
- * The OpenSpace Android SDK is protected by © Crown copyright – Ordnance Survey 2013.[https://github.com/OrdnanceSurvey]
- *
- * All rights reserved (subject to the BSD licence terms as follows):.
- *
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided
- * with the distribution.
- *
- * Neither the name of Ordnance Survey nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
- *
- */
+* OpenSpace Android SDK Licence Terms
+*
+* The OpenSpace Android SDK is protected by © Crown copyright – Ordnance Survey 2013.[https://github.com/OrdnanceSurvey]
+*
+* All rights reserved (subject to the BSD licence terms as follows):.
+*
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+*
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided
+* with the distribution.
+*
+* Neither the name of Ordnance Survey nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+* INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+*
+*/
 package uk.co.ordnancesurvey.osmobilesdk.raster.network;
 
 import android.content.Context;
@@ -34,9 +34,9 @@ import rx.functions.Action1;
 import rx.functions.Func1;
 
 /**
- * Class the observe network connectivity changes and record if network access is possible
- * TODO: add a subscriber to start?
- */
+* Class the observe network connectivity changes and record if network access is possible
+* TODO: add a subscriber to start?
+*/
 public class NetworkStateMonitor {
 
     private static final int[] NETWORK_TYPES = {
---
+++ OSMapAndroid/raster/src/main/java/uk/co/ordnancesurvey/osmobilesdk/raster/network/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains network focused, such as
+ * {@link uk.co.ordnancesurvey.osmobilesdk.raster.network.NetworkStateMonitor}.
+ */
+package uk.co.ordnancesurvey.osmobilesdk.raster.network;

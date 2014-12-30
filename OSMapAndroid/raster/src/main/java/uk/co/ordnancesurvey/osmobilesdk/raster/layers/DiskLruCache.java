a84a61022461d0abe4c13a13d339b1e24fb110ee
Jim Reeves <jim@jsrsoftware.com>
2014-12-30 18:00:15 +0000

    Add licences to classes

--- OSMapAndroid/raster/src/main/java/uk/co/ordnancesurvey/osmobilesdk/raster/layers/DiskLruCache.java
+++ OSMapAndroid/raster/src/main/java/uk/co/ordnancesurvey/osmobilesdk/raster/layers/DiskLruCache.java
@@ -41,7 +41,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package uk.co.ordnancesurvey.osmobilesdk.raster.layers;
 
 import android.util.Log;
